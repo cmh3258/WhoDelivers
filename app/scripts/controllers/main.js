fed2c9c8a68dd118ef98e7eca67c54d47d7a97c6
chris <chume@gmail.com>
2016-01-26 22:33:03 -0600

    austin restaurants + styling

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,6 +14,84 @@ angular.module('whodeliversApp')
     $scope.viewOtherOptions = false;
     $scope.results = [];
 
+    $scope.austinRestaurants = [
+      {
+        'name':'Cabo Bob\'s',
+        'img':'../images/restaurant_images/cabobob.jpg',
+        'fsid':''
+      },
+      {
+        'name':'Chilantro BBQ',
+        'img':'../images/restaurant_images/chi.jpg',
+        'fsid':''
+      },
+      {
+        'name':'Fat Sal\'s',
+        'img':'../images/restaurant_images/fatsal.jpg',
+        'fsid':''
+      },
+      {
+        'name':'Fire Bowl Cafe',
+        'img':'../images/restaurant_images/firebowl.jpg',
+        'fsid':''
+      },
+      {
+        'name':'Homeslice',
+        'img':'../images/restaurant_images/homeslice.jpg',
+        'fsid':''
+      },
+      {
+        'name':'Austin Habibi',
+        'img':'../images/restaurant_images/habibi.jpg',
+        'fsid':''
+      },
+      {
+        'name':'Juice Austin',
+        'img':'../images/restaurant_images/juice.jpg',
+        'fsid':''
+      },
+      {
+        'name':'Koriente',
+        'img':'../images/restaurant_images/korien.jpg',
+        'fsid':''
+      },
+      {
+        'name':'Milto\'s',
+        'img':'../images/restaurant_images/milto.jpg',
+        'fsid':''
+      },
+      {
+        'name':'Noble Pig',
+        'img':'../images/restaurant_images/noble.jpg',
+        'fsid':''
+      },
+      {
+        'name':'Pei Wei',
+        'img':'../images/restaurant_images/pei.jpg',
+        'fsid':''
+      },
+      {
+        'name':'Qdoba',
+        'img':'../images/restaurant_images/qdoba.jpg',
+        'fsid':''
+      },
+      {
+        'name':'Schlotzsky\'s',
+        'img':'../images/restaurant_images/schlotz.jpg',
+        'fsid':''
+      },
+      {
+        'name':'Torchy\'s',
+        'img':'../images/restaurant_images/torchy.jpg',
+        'fsid':''
+      },
+      {
+        'name':'Whataburger',
+        'img':'../images/restaurant_images/whata.jpg',
+        'fsid':''
+      },
+    ];
+
     $scope.searchForRestaurants = function(){
       console.log('hi');
       if($scope.address === 'Success'){
